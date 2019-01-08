a372f2cca1fa29d28c62557fffb9af60b6ba28ca
Angela Wang <angeladdpiano@gmail.com>
2019-01-08 15:39:15 -0800

    AW-initial commit. Landing page, form and saving completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'images/index'
+  get 'images/new'
+  get 'images/create'
+  resources :images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
+
 end
